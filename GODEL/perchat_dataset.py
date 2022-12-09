542431888e52bd3dfcec18d2e5e9709fb1b4c2fb
Xiangrong Xu <xiangrongxu@josousugatas-MacBook-Pro.local>
2022-12-08 19:46:56 -0500

    first commit

---
+++ GODEL/perchat_dataset.py
@@ -0,0 +1,53 @@
+import datasets
+import jsonlines
+
+"""Corpus for PerChatPretrain"""
+
+_DESCRIPTION = """\
+PerChatPretrain
+"""
+
+_CITATION = """\
+PerChatPretrain
+"""
+
+_WEBPAGE = ""
+
+class PerChatPretrain(datasets.GeneratorBasedBuilder):
+    """RedditPretrain"""
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "Context": datasets.Value("string"),
+                    "Response": datasets.Value("string"),
+                    "Knowledge": datasets.Value("string")
+                }
+            ),
+            homepage=_WEBPAGE,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        train_path = './perchat/perchat_train.jsonl'
+        validation_path = './perchat/perchat_valid.jsonl'
+        test_path = './perchat/perchat_test.jsonl'
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={
+                                    "filepath": train_path}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={
+                                    "filepath": validation_path}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={
+                                    "filepath": test_path}),
+        ]
+
+    def _generate_examples(self, filepath):
+        key = 0
+        with open(filepath, "r", encoding="utf-8") as reader:
+            for item in jsonlines.Reader(reader):
+                yield key, item
+                key += 1
+
